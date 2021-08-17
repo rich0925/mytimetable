291d53ef23197007f2e1ce5666ae77a65587bd2e
名前 <ec2-user@ip-172-31-19-185.us-east-2.compute.internal>
2021-08-17 00:40:32 +0000

    20210817 commit!1

--- app/models/gyou.rb
+++ app/models/gyou.rb
@@ -1,4 +1,13 @@
 class Gyou < ApplicationRecord
   has_many :artists
   scope :agyou, -> {find_by(gyou: "あ行")}
+  scope :kagyou, -> {find_by(gyou: "か行")}
+  scope :sagyou, -> {find_by(gyou: "さ行")}
+  scope :tagyou, -> {find_by(gyou: "た行")}
+  scope :nagyou, -> {find_by(gyou: "な行")}
+  scope :hagyou, -> {find_by(gyou: "は行")}
+  scope :magyou, -> {find_by(gyou: "ま行")}
+  scope :yagyou, -> {find_by(gyou: "や行")}
+  scope :ragyou, -> {find_by(gyou: "ら行")}
+  scope :wagyou, -> {find_by(gyou: "わ行")}
 end
